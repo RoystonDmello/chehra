474c99409bbdaddd10852c5acf61f9ad41b1ab25
TejasBhitle <bhitletejas@gmail.com>
2017-12-17 23:17:11 +0530

    Initial setup

--- src/AttendanceManager/settings.py
+++ src/AttendanceManager/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local
+    'managerApp',
+
+    # third party
+
 ]
 
 MIDDLEWARE = [
---
+++ src/managerApp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+
+# Create your views here.
+def index(request):
+    return JsonResponse({'text': 'hello This is index page',
+                         'status': '200 OK'})
