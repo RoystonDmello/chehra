ec7593636614c9bf890e51c426fd569b77207c4d
TejasBhitle <bhitletejas@gmail.com>
2018-01-04 00:18:23 +0530

    Changes in Register and Login Views

--- src/apiApp/auth/serializers.py
+++ src/apiApp/auth/serializers.py
@@ -1,5 +1,12 @@
 from ..models import Student, StudentImage
 from rest_framework.serializers import ModelSerializer
+from django.contrib.auth.models import User
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
 
 
 class StudentSerializer(ModelSerializer):
