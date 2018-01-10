511c540508b362ec61b6ae8ac6fdbd01e04f114b
TejasBhitle <bhitletejas@gmail.com>
2018-01-10 22:35:40 +0530

    url added to access /media root

--- src/AttendanceManager/urls.py
+++ src/AttendanceManager/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import include, url
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'api/', include("apiApp.urls")),
     url(r'api-auth/', include('rest_framework.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
